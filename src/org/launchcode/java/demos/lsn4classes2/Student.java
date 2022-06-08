2127872394b7049cedba67adb8795392cb4da58a
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-06-07 18:33:22 -0600

    Created 2 instance methods for Student class. Created toSTring and equals method overrides for Student class and COurse class. Completed Chapter 5 (lsn4classes2) exercise.

--- src/org/launchcode/java/demos/lsn4classes2/Student.java
+++ src/org/launchcode/java/demos/lsn4classes2/Student.java
@@ -30,20 +30,54 @@ public class Student {
 
 
      //TODO: Uncomment and complete the getGradeLevel method here:
-//    public String getGradeLevel() {
-//        // Determine the grade level of the student based on numberOfCredits
-//    }
+    public static String getGradeLevel(int credits) {
+        if (credits >= 90) {
+            return "Senior";
+        } else if (credits >= 60) {
+            return "Junior";
+        } else if (credits >= 30) {
+            return "Sophomore";
+        } else {
+            return "Freshman";
+        }
+        // Determine the grade level of the student based on numberOfCredits
+    }
 
     // TODO: Complete the addGrade method.
     public void addGrade(int courseCredits, double grade) {
         // Update the appropriate fields: numberOfCredits, gpa
+        double currentTotalQualityScore = this.gpa * this.numberOfCredits;
+        double newTotalQualityScore = currentTotalQualityScore + (courseCredits * grade);
+        this.numberOfCredits = this.numberOfCredits + courseCredits;
+        this.gpa = newTotalQualityScore / this.numberOfCredits;
     }
 
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather
     //  than just the class fields.
+    public String toString(){
+        String studentReport = ("Name: " + this.name + " Grade Level: " + this.getGradeLevel(this.numberOfCredits) + " Student ID: " + this.studentId + " Number of Credits: " + this.numberOfCredits + " GPA: " + this.gpa);
+        return studentReport;
+    }
+
+
 
     // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
     //  Student objects equal.
+
+    public boolean equals(Object toBeCompared){
+        if (toBeCompared == this){
+            return true;
+        }
+        if (toBeCompared == null){
+            return false;
+        }
+        if (toBeCompared.getClass() != this.getClass()){
+            return false;
+        }
+
+        Student theStudent = (Student) toBeCompared;
+        return theStudent.getStudentId() == getStudentId();
+    }
 
     public String getName() {
         return name;
@@ -86,4 +120,5 @@ public class Student {
         sally.addGrade(25, 3.8);
         System.out.println(sally);
     }
+
 }
